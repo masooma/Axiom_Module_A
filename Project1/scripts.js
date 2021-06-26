9938851001e98bf66a9ffcd4758eef31c7e049b7
masooma <masoomalc@gmail.com>
2021-06-26 12:41:15 +0500

    javascript being updated

--- Project1/scripts.js
+++ Project1/scripts.js
@@ -7,6 +7,31 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 //All functions
+
+// Function to check required field
+function checkIfRequired(inputArray){
+    inputArray.forEach( function(input) {
+        if(!input.value){
+            showError(input, "This field is required");
+        }
+        else {
+            showSuccess(input);
+        }
+    });
+}
+
+//Function to check the length of input
+function checkInputLength(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${input.id} needs to be atleast ${min} characters`);
+    }
+    else if(input.value.length > max){
+        showError(input, `${input.id} needs to be less than ${max} characters`);
+    }
+    else{
+        showSuccess(input);
+    }
+}
 // Function to show Error
 function showError(input, message){
 
@@ -22,39 +47,37 @@ function showSuccess(input){
     formControl.className = 'form-control success';
 }
 // Function to Validate Email
-function ValidateEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    }
+    else{
+        showError(input, 'Enter a valid email');
+    }
+}
+
+//Function to check if password and confirm password are same
+
+function checkPass(pass, pass2){
+
+    comp = pass.value.localeCompare(pass2.value);
+    if(comp == 0){
+        showSuccess(pass2);
+    }
+    else{
+        showError(pass2, 'Passwords are not identical');
+    }
 }
 
 // Event Listener for submit button
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if (!username.value){
-        showError(username, 'Username is required');
-    }
-    else{
-        showSuccess(username);
-    }
-    if (!email.value){
-        showError(email, 'Email is required');
-    } else if(!ValidateEmail(email.value)){
-        showError(email, 'Email is invalid');
-    }
-    else{
-        showSuccess(email);
-    }
-    if (!password.value){
-        showError(password, 'Password is required');
-    }
-    else{
-        showSuccess(password);
-    }
-    if (!password2.value){
-        showError(password2, 'Confirm password is required');
-    }
-    else{
-        showSuccess(password2);
-    }
+    checkIfRequired([username,email,password,password2]);
+    checkInputLength(username,3,10);
+    checkInputLength(password,6,30);
+    checkEmail(email);
+    checkPass(password, password2);
+
 })
