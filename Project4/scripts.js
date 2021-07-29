8cd712ab431b495c956ea9a47f00fe496971b9ec
masooma <masoomalc@gmail.com>
2021-07-29 23:22:01 +0500

    added JS to show conversion rates of all countries

--- Project4/scripts.js
+++ Project4/scripts.js
@@ -7,10 +7,13 @@ const target_currency = document.getElementById('target-currency');
 const swap = document.getElementById('swap');
 const calculate_r = document.getElementById('calculate');
 const xrate = document.getElementById('rate');
+const text_area = document.getElementById('text_area');
+const all_count = document.getElementById('all_countries');
 
 // Event Listeners
 
 calculate_r.addEventListener('click', calculate);
+all_count.addEventListener('click', all_count_conversionRates);
 swap.addEventListener('click', () => {
     const temp_v = base_country.value;
     base_country.value = target_country.value;
@@ -21,9 +24,9 @@ swap.addEventListener('click', () => {
 //function to calculate the exchange rate
 function calculate(){
 
-    b_country = base_country.value;
-    t_country = target_country.value;
-    b_currency = base_currency.value;
+    const b_country = base_country.value;
+    const t_country = target_country.value;
+    const b_currency = base_currency.value;
 
     fetch(`https://v6.exchangerate-api.com/v6/dd884050c638c6acf706ab73/pair/${b_country}/${t_country}`)
         .then( res => res.json())
@@ -32,4 +35,18 @@ function calculate(){
             target_currency.value = converted_currency;
             xrate.innerHTML = `Conversion rate = ${data.conversion_rate}`;
         })        
+};
+
+// function to show exchange rates of all countries
+function all_count_conversionRates(){
+
+    const b_country = base_country.value;
+    fetch(`https://v6.exchangerate-api.com/v6/dd884050c638c6acf706ab73/latest/${b_country}`)
+        .then(res => res.json())
+        .then(data => {
+            const conversion_r = data.conversion_rates;
+            //console.log(conversion_r);
+            text_area.value = JSON.stringify(conversion_r);
+        })
+    
 };
